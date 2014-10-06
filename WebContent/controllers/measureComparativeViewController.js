b7df6c10f505b62c56f1f07da0bb0729a1376168
jimcurry <jim.j.curry@gmail.com>
2014-10-06 11:53:20 -0400

    Further work on the Measure Comparative View - Added stub View selection - Sized the two containers for the embedded Cognos reports - Added stubbed tab navigation - Added placeholder domainService

--- WebContent/controllers/measureComparativeViewController.js
+++ WebContent/controllers/measureComparativeViewController.js
@@ -1,16 +1,40 @@
 var dashboardApp = angular.module("dashboardApp");
 
-dashboardApp.controller("measureComparativeViewController", function($scope, $http, $state, $stateParams, $location, ngDialog, userService, reportingPeriodService, networkHierarchyService, CognosMashupURL, CognosNamespace) {
+dashboardApp.controller("measureComparativeViewController", function($scope, $sce, $http, $state, $stateParams, $location, ngDialog, userService, reportingPeriodService, networkHierarchyService, domainService, CognosMashupURL, CognosNamespace) {
 
 	$scope.user = userService.user;
-	
 	$scope.network = networkHierarchyService.network;
 	$scope.reportingPeriod = reportingPeriodService.reportingPeriod;
+	$scope.domainData = domainService.domainData;
 	
 	$scope.selectReportingPeriod = function(selectedValue) {
 		 reportingPeriodService.setSelectedItemByUseValue(selectedValue);
 		 $scope.reportingPeriod = reportingPeriodService.reportingPeriod;
 		 //$state.go('measureComparativeView', {'reportingPeriod' : selectedValue});
+	};
+
+    $scope.viewList = [
+                            {name:"Measure Comparative View", id: "MCV"},
+                            {name:"Domain Comparative View", id: "DCV"}
+                           ];
+    $scope.selectedView = $scope.viewList[0];	
+
+	$scope.isSelected = function(domain) {
+		if ($scope.domainData.selectedDomain.id == domain.id) {
+			return true;
+		} else {
+			return false;
+		}
+	};
+
+    $scope.loadTab = function(domain) {
+		$scope.domainData.selectedDomain = domain;
+			
+		$scope.summaryPaneContent = "<p>" + domain.name + "</p>";
+	};
+
+	$scope.renderHtml = function(html_code){
+		return $sce.trustAsHtml(html_code);
 	};
 
 	$scope.parm1 = $stateParams.parm1;
